a2a851101b0c93e1f100d007ecd963ada88e61fd
Daniel X Moore <yahivin@gmail.com>
2015-01-27 09:51:57 -0800

    :white_check_mark::pig: Updated in browser at strd6.github.io/editor

---
+++ master.json.js
@@ -0,0 +1,43 @@
+window["STRd6/hello-world:master"]({
+  "source": {
+    "LICENSE": {
+      "path": "LICENSE",
+      "content": "The MIT License (MIT)\n\nCopyright (c) 2015 Daniel X Moore\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n\n",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "README.md": {
+      "path": "README.md",
+      "content": "# hello-world\nHello World\n",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "main.coffee": {
+      "path": "main.coffee",
+      "content": "alert \"Hello\"\n",
+      "mode": "100644"
+    }
+  },
+  "distribution": {
+    "main": {
+      "path": "main",
+      "content": "(function() {\n  alert(\"Hello\");\n\n}).call(this);\n",
+      "type": "blob"
+    }
+  },
+  "progenitor": {
+    "url": "http://pketh.org/HyperWeb/?repo=STRd6/hello-world"
+  },
+  "entryPoint": "main",
+  "repository": {
+    "branch": "master",
+    "default_branch": "master",
+    "full_name": "STRd6/hello-world",
+    "homepage": null,
+    "description": "Hello World",
+    "html_url": "https://github.com/STRd6/hello-world",
+    "url": "https://api.github.com/repos/STRd6/hello-world",
+    "publishBranch": "gh-pages"
+  },
+  "dependencies": {}
+});
